aa93896d36b5ebc0f91b95730385f2da3f36af94
Aodj05 <aodj05@msn.com>
2022-09-01 16:48:30 -0400

    final

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,99 +1,110 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Justin J. Lugo",
-    "role": "Web Developer",
-    "linkedinId":"https://www.linkedin.com/in/justinlugo/",
-    "roleDescription": " I am a self-taught web developer where I enjoy learning various coding languages, tools and practices.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/justinlugo/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/Aodj05",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/Aodj05",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a contractor Camera Operator, learning fullstack web development. I find it compelling and rewarding to learn and grow through all the language and tools to produce something.",
-    "address":"Orlando, Fl",
-    "website":" https://rbhatia46.github.io",
-    "phone" : " 407-462-8903",
-    "education":[
+  "imagebaseurl":"",
+  "name": "Justin J. Lugo",
+  "role": "Web Developer",
+  "linkedinId":"https://www.linkedin.com/in/justinlugo/",
+  "roleDescription": " I am a self-taught web developer where I enjoy learning various coding languages, tools and practices.",
+  "socialLinks":[
       {
-        "UniversityName":"University of Central Florida",
-        "specialization":"Interdisciplinary Studies",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2014",
-        "Achievements":"Bachelor of Science: Digital Media, Computational Science, Biology, Mythology"
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/justinlugo/",
+        "className":"fa fa-linkedin"
       },
       {
-        "UniversityName":"Valencia College",
-        "specialization":"General Education",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2009",
-        "Achievements":"Statistics, Humanities"
+        "name":"github",
+        "url":"http://github.com/Aodj05",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"twitter",
+        "url":"http://twitter.com/Aodj05",
+        "className":"fa fa-twitter"
       }
     ],
-    "work":[
-      {
-        "CompanyName":"Grid Iron Productions",
-        "specialization":"Onsite camera and equipment setup and use",
-        "MonthOfLeaving":"current",
-        "YearOfLeaving":"current",
-        "Achievements":"Steady clientele"
-      },
-      {
-        "CompanyName":"StreamSportsLLC",
-        "specialization":"Setup and use of equipment, work with highlight reels",
-        "MonthOfLeaving":"dec",
-        "YearOfLeaving":"2017",
-        "Achievements":"Identified and recorded important pieces of footage"
-      }
-    ],
-    "skillsDescription":"Skills",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      },
-      {
-        "skillname":"Ruby on Rails"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/coffee.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/console.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/girl.jpg"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/judah.jpg"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "aboutme":"I am currently a contractor Camera Operator, learning fullstack web development. I find it compelling and rewarding to learn and grow through all the language and tools to produce something.",
+  "address":"2045 Excalibur Dr. Orlando, Fl 32822",
+  "website":"",
+  "phone" : " 407-462-8903",
+  "email" : "aodj05@gmail.com",
+  "education":[
+    {
+      "UniversityName":"University of Central Florida",
+      "specialization":"Interdisciplinary Studies",
+      "MonthOfPassing":"Aug",
+      "YearOfPassing":"2014",
+      "Achievements":"Bachelor of Science: Digital Media, Computational Science, Biology, Mythology"
+    },
+    {
+      "UniversityName":"Valencia College",
+      "specialization":"General Education",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2009",
+      "Achievements":"Statistics, Humanities"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Grid Iron Productions",
+      "specialization":"Onsite camera and equipment setup and use",
+      "MonthOfLeaving":"current",
+      "YearOfLeaving":"current",
+      "Achievements":"Steady clientele"
+    },
+    {
+      "CompanyName":"StreamSportsLLC",
+      "specialization":"Setup and use of equipment, work with highlight reels",
+      "MonthOfLeaving":"dec",
+      "YearOfLeaving":"2017",
+      "Achievements":"Identified and recorded important pieces of footage"
+    }
+  ],
+  "skillsDescription":"Skills",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Ruby on Rails"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"RubyChess GitHub",
+      "description":"Simple chess game made from Ruby",
+      "imgurl":"./personal-website/images/portfolio/RubyChess.png",
+      "url": "https://github.com/Aodj05/RubyChess"
+    },
+    {
+      "name":"Cutesy Card Safari",
+      "description":"Animal themed card game made with HTML5, CSS, and vanilla JavaScript",
+      "imgurl":"./personal-website/images/portfolio/CCS.png",
+      "url": "https://aodj05.github.io/CardGameJS/"
+    },
+    {
+      "name":"Turned Based Battle",
+      "description":"Reactjs turn based battle game",
+      "imgurl":"./personal-website/images/portfolio/TBB.png",
+      "url":"https://aodj05.github.io/turnbasedbattle/"
+    },
+    {
+      "name":"MeTube",
+      "description":"Youtube clone using Reactjs, MongoDB, and firebase",
+      "imgurl":"./personal-website/images/portfolio/MeTube.png",
+      "url":"https://aodj05.github.io/metube/"
+    },
+    {
+      "name":"DelayedGramp GitHub",
+      "description":"Instagram clone made with Ruby on Rails 7",
+      "imgurl":"./personal-website/images/portfolio/Dg.png",
+      "url":"https://github.com/Aodj05/delayed_gramps"
+    }
+  ]
+}
+
+export default resumeData
